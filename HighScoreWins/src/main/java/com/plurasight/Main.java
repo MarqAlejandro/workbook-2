cb847b4136d9f0a5deadafe8acd3c2dce907f052
MarqAlejandro <alejandromarq3@gmail.com>
2025-04-15 16:43:53 -0700

    Update Main.java

--- HighScoreWins/src/main/java/com/plurasight/Main.java
+++ HighScoreWins/src/main/java/com/plurasight/Main.java
@@ -2,9 +2,6 @@ package com.plurasight;
 
 public class Main {
     public static void main(String[] args) {
-        
-      
-
         ScoreChecker scoreChecker = new ScoreChecker();
 
         scoreChecker.andTheWinnerIs();
