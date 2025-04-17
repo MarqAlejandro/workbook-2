d3a02766518fc2db3d63a784b30853cd0970c959
MarqAlejandro <alejandromarq3@gmail.com>
2025-04-17 11:21:58 -0700

    completed Section 2-1 exercise 1
    CellPhoneApp is done

---
+++ CellPhoneApplication/src/main/java/com/plurasight/Cellphone.java
@@ -0,0 +1,93 @@
+package com.plurasight;
+
+import java.util.Scanner;
+
+public class Cellphone {
+
+    private int serialNumber;                               //initialize variables
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    Scanner scanner = new Scanner(System.in);               //global scanner object
+
+    public Cellphone() {                                    //constructor
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    public void turnOn(){
+        setUp();
+        displayInfo();
+
+    }
+    public void setUp(){                                    //prompts user for input and uses setters
+        System.out.print("What is the serial number? ");
+        setSerialNumber(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.print("What model is the phone?" );
+        setModel(scanner.nextLine());
+
+        System.out.print("Who is the carrier? ");
+        setCarrier(scanner.nextLine());
+
+        System.out.print("What is the phone number? ");
+        setPhoneNumber(scanner.nextLine());
+
+        System.out.print("Who is the owner of the phone? ");
+        setOwner(scanner.nextLine());
+
+    }
+    public void displayInfo(){
+        System.out.println("\nHello, " + getOwner());
+        System.out.println("This is your new " + getModel() + " with serial number " + getSerialNumber());
+        System.out.println("Your carrier is " + getCarrier() + " and your phone number is " + getPhoneNumber());
+        System.out.println("Enjoy your new phone!");
+    }
+
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
---
+++ CellPhoneApplication/src/main/java/com/plurasight/Main.java
@@ -0,0 +1,11 @@
+package com.plurasight;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Cellphone cellphone = new Cellphone();
+
+        cellphone.turnOn();
+    }
+}
